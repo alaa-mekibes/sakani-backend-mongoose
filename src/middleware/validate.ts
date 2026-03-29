ba08c1c63f6ded9e3cbb83f03df10e0059afcae9
Alaa Mekibes <alaa.mekibes@proton.me>
2026-03-29 20:23:43 +0100

    fix zod path

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from 'express';
-import { z } from '../lib/zod';
+import { z } from 'zod';
 import { ApiResponse } from '../utils/apiResponse';
 
 export const validate =
